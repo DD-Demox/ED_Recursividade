c41279dd6f2546ce3dc01b3110b128c2c33d8c71
Armando Asch <armandoasch@gmail.com>
2023-03-27 23:45:57 -0300

    agora com memoization

--- 2.py
+++ 2.py
@@ -1,9 +1,13 @@
+from functools import cache
+
+@cache
 def fibonacci(nTermos):
-    if nTermos == 1:
+    if nTermos <= 1:
+        print(1)
         return 1
-    elif nTermos <= 0:
-        return 0
-    return fibonacci(nTermos-1) + fibonacci(nTermos-2)
+    fib = fibonacci(nTermos-1) + fibonacci(nTermos-2)
+    print(fib)
+    return fib
 
 if __name__ == '__main__':
-    print(fibonacci(2))
+    fibonacci(10)
